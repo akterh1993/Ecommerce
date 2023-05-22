2f4e2767c629f2bdd1f4945fc38db44012e5ed1b
akterh1993 <akterh1993@gmail.com>
2023-05-22 10:59:59 +0600

    somthing add admin route

--- admin/src/router/routes/adminRoutes.js
+++ admin/src/router/routes/adminRoutes.js
@@ -9,6 +9,7 @@ const DeactiveSeller = lazy(() => import("../../views/admin/DeactiveSeller"));
 const SellerRequest = lazy(() => import("../../views/admin/SellerRequest"));
 const SellerDetails = lazy(() => import("../../views/admin/SellerDetails"));
 const ChatSeller = lazy(() => import("../../views/admin/ChatSeller"));
+const OrderDetails = lazy(() => import("../../views/admin/OrderDetails"));
 
 export const adminRoutes = [
     {
@@ -54,6 +55,11 @@ export const adminRoutes = [
     {
         path: 'admin/dashboard/seller-chat',
         element: <ChatSeller />,
+        role : 'admin'
+    },
+    {
+        path: 'admin/dashboard/order/details/:orderId',
+        element: <OrderDetails />,
         role : 'admin'
     },
 ]
