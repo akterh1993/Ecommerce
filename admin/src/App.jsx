e6d4eaef74b949a6de95762950c59ea7e867972d
akterh1993 <akterh1993@gmail.com>
2023-05-31 12:04:09 +0600

    update

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,28 +1,27 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 
 import { useEffect, useState } from "react";
-import { useSelector, useDispatch} from 'react-redux'
+// import { useSelector, useDispatch} from 'react-redux'
 import Router from './router/Router'
 import publicRoutes from './router/routes/publicRoutes'
 import { getRoutes } from "./router/routes";
-import { get_user_info } from "./store/Reducers/authReducer";
+// import { get_user_info } from "./store/Reducers/authReducer";
 
 
 function App() {
-  const dispatch = useDispatch()
-  const { token } = useSelector(state => state.auth)
+  // const dispatch = useDispatch()
+  // const { token } = useSelector(state => state.auth)
   const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
 
   useEffect(() => {
     const routes = getRoutes()
     setAllRoutes([...allRoutes, routes])
   }, [])
-  useEffect(()=>{
-    if (token) {
-      dispatch(get_user_info())
+  // useEffect(()=>{
+  //   if (token) {
+  //     dispatch(get_user_info())
       
-    }
-  }, [token])
+  //   }
+  // }, [token])
 
   return <Router allRoutes={allRoutes} />
 }
